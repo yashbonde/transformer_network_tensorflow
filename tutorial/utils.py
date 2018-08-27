4a81c645e327d79f128bf9f3f985823ef1f2c8b7
Yash Bonde <bonde.yash97@gmail.com>
2018-08-27 15:52:48 +0530

    Corrected errors

--- tutorial/utils.py
+++ tutorial/utils.py
@@ -1,18 +1,13 @@
 # importing the dependencies
-import argparse
 import numpy as np
-import tensorflow as tf
 
-def positional_encoding(self, pos):
+def positional_encoding(pos, e_dim):
 	# @TODO: optimise this piece of code
 	pos_embed = []
-	for i in range(self.D_MODEL):
-		angle = pos/(10**(8*i/self.D_MODEL))
+	for i in range(e_dim):
+		angle = pos/(10**(4*i/e_dim))
 		if i%2 == 0:
 			pos_embed.append(np.sin(angle))
 			continue
 		pos_embed.append(np.cos(angle))
 	return np.array(pos_embed, dtype = np.float32)
-
-def load_data(filename):
-	return pickle.load(open(filename, 'rb'))
